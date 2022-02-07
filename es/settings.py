ae705ca559fde8d12db3898b1e25da741e67997b
Jovan Bojovic <jovanbojovic999@gmail.com>
2022-02-07 12:39:13 +0100

    settings

--- es/settings.py
+++ es/settings.py
@@ -18,7 +18,6 @@ import whitenoise
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -53,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+django_heroku.settings(locals())
 
 ROOT_URLCONF = 'es.urls'
 
